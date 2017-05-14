b7800ef80c2bb3955608042e9acdd6b2b497576b
ravil <ravil.mobile.com@gmail.com>
2017-05-14 14:06:58 +0200

    G++ flags were changed. Now simulation is runing with O1 optimization

--- WS1/main.c
+++ WS1/main.c
@@ -6,6 +6,7 @@
 #include "sor.h"
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 /**
  * The main operation reads the configuration file, initializes the scenario and
@@ -69,7 +70,7 @@ int main(int argn, char** args){
     }
     else {
 
-        // Abort execution if number of input parameters is wrong
+        // Abort execution if the number of input parameters is wrong
         printf( "\nERROR: The number of input parameters is wrong\n\n" );
 
         printf( "HINT:   You've probably forgot to pass the input file" );
@@ -82,29 +83,29 @@ int main(int argn, char** args){
 
 
 
-	// declaring the matrices
+	// declare the matrices
 	double **U, **V, **P;
 	
 	double **RS, **F, **G;
 
-	// Declaring variables for the parameters that will be read from the input file
-	double Re, // Reynalds number
-	       UI, // initial values of the U velocity component
-	       VI, // initial values of the V velocity component
-	       PI, // initial values of the pressure
-	       GX, //acceleration due to gravity in x-direction
-	       GY, //acceleration due to gravity in y-direction
-	       t_end, // final of simulation time
-	       xlength, // dimension of the problem in x-direction
-	       ylength, // dimension of the problem in y-direction
-	       dt, // time step
-	       dx, //Grid size in x-direction
-	       dy, //Grid size in y-direction
+	// Declare variables for the parameters that will be read from the input file
+	double Re,          // Reynalds number
+	       UI,          // initial values of the U velocity component
+	       VI,          // initial values of the V velocity component
+	       PI,          // initial values of the pressure
+	       GX,          // acceleration due to gravity in x-direction
+	       GY,          // acceleration due to gravity in y-direction
+	       t_end,       // final of simulation time
+	       xlength,     // dimension of the problem in x-direction
+	       ylength,     // dimension of the problem in y-direction
+	       dt,          // time step
+	       dx,          //Grid size in x-direction
+	       dy,         //Grid size in y-direction
 	       alpha,
-	       omg, // SOR-relaxation factor
-	       tau, //safety factor
-	       eps, // numerical precision
-	       dt_value; // time for output
+	       omg,         // SOR-relaxation factor
+	       tau,         //safety factor
+	       eps,         // numerical precision
+	       dt_value;    // time for output
 	
 	int  imax, //number of cells x-direction
 	     jmax, //number of cells y-direction
@@ -137,7 +138,6 @@ int main(int argn, char** args){
 	                 &eps,
 	                 &dt_value );
 
-	// TODO: figure out that how to use that value: dt_value
 
 	// Allocating memory for matrices: U, V and P
 	U = matrix(0, imax + 1, 0, jmax + 1);
@@ -169,6 +169,8 @@ int main(int argn, char** args){
 	int TimeStepNumber = 0;
 	int SolverIterationNumber = 0;
 	const char* OUPUT_FILE_NAME = "Cavity100";
+	double FrameCounter = 0.0;
+
 
     // Initializing timer
     clock_t Begin = clock();
@@ -176,24 +178,16 @@ int main(int argn, char** args){
 	//The main while loop that iterates over time
 	while ( t < t_end ) {
 
-		//DELETE Comment Can tau be = 0?? or only > 0
-		//Calcaulte the time step
+		//Calcaulte the adaptive time step
 		if (tau >= 0.0)
 		    calculate_dt(Re, tau, &dt, dx, dy, imax, jmax, U, V);
-		else
-		    dt = dt_value;
 
-        //printf("dt: %6.20f  \n", dt); // DEBUGGING
 
 		// Set the boundary for U and V
 		boundaryvalues(imax, jmax, U, V);
 
 		// Calcualte F and G
 		calculate_fg(Re, GX, GY, alpha, dt, dx, dy, imax, jmax, U, V, F, G);
-
-
-		//double tmp = absoluteMaxInArray( U, imax, jmax );   // DEBUGGING
-		//printf("dt: %6.20f  \n", tmp); // DEBUGGING
 
 
 		// Calculate the RHS of Pressure Poisson Equation
@@ -208,35 +202,42 @@ int main(int argn, char** args){
 		    //Perform one SOR iteration
 			sor (omg, dx, dy, imax, jmax, P, RS, &Residual);
 			SolverIterationNumber++;
-			//printf("interation: %d   ", iternum ); // DEBUGGING
-			//printf("residual: %6.20f\n", res ); // DEBUGGING
 		}
+
 
 		// Update the velocities U and V
 		calculate_uv(dt, dx, dy, imax, jmax, U, V, F, G, P);
 
 
-		//DELETE Comment We can also write the vtk at the end
-		//Output vtk file every 50th timestep
-	if ( TimeStepNumber % 50 == 0 ) write_vtkFile(  OUPUT_FILE_NAME,
-		                                            TimeStepNumber,
-		                                            xlength,
-		                                            ylength,
-		                                            imax,
-		                                            jmax,
-		                                            dx,
-		                                            dy,
-		                                            U,
-		                                            V,
-		                                            P);
+        // write result to a vtk file each ${dt_value} seconds
+        if ( floor( FrameCounter / dt_value ) ) {
+
+                write_vtkFile(  OUPUT_FILE_NAME,
+                                TimeStepNumber,
+                                xlength,
+                                ylength,
+                                imax,
+                                jmax,
+                                dx,
+                                dy,
+                                U,
+                                V,
+                                P );
+
+                FrameCounter = 0.0;
+	}
+
 
 		//Update the loop variables
 		t += dt;
 		++TimeStepNumber;
+		FrameCounter += dt;
 	}
-	
+
+	// display the output information
     clock_t End = clock();
     double ConsumedTime = (double)( End - Begin ) / CLOCKS_PER_SEC;
+
     printf("\n\nComputational time: %f sec\n", ConsumedTime);
     printf("Mesh size [ imax x jmax ]: %d x %d \n", imax, jmax ); 
     printf("Relaxation factor [ omega ]: %4.4f \n\n", omg );
@@ -247,11 +248,9 @@ int main(int argn, char** args){
 	free_matrix(P, 0, imax + 1, 0, jmax + 1);
 
 	free_matrix(RS, 0, imax + 1, 0, jmax + 1);
-	free_matrix(F, 0, imax, 0, jmax + 1);
-	free_matrix(G, 0, imax + 1, 0, jmax);
+	free_matrix(F, 0, imax + 1, 0, jmax + 1);
+	free_matrix(G, 0, imax + 1, 0, jmax + 1);
 
-    // DEBUGGING
-	// return -1;
 	return 0;
 }
 

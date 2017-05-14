58179deb36b33ece45b19d383cff1b8b8552bede
ravi-kislaya <ga83tum@mytum.de>
2017-05-14 23:16:52 +0200

    1. Report added 2.Commenting of code completed

--- WS1/boundary_val.c
+++ WS1/boundary_val.c
@@ -8,16 +8,18 @@ void boundaryvalues(int imax,//Grid size in x-direction
 {
 	int i, j;
 	
-	//To Do: describe the boundary conditions using comments
-	//To Do: setup lid velocity
+	
 	for(i = 1; i <= imax; ++i) {
+		//No-slip boundary conditions
 		V[i][0] = 0;
 		V[i][jmax] = 0;
 		U[i][0] = -1.0 * U[i][1];
-		U[i][jmax+1] = 2.0 - U[i][jmax];
+		//Following boundary condition which includes lid velocity
+		U[i][jmax+1] = 2.0 - U[i][jmax]; 
 	}
 	
 	for(j = 1; j <= jmax; ++j) {
+		//No-slip boundary conditions
 		U[0][j] = 0;
 		U[imax][j] = 0;
 		V[0][j] = -1.0 * V[1][j];

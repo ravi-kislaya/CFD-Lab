229d23497819e442d7e5bfcb4902783dda32627f
ravi-kislaya <ga83tum@mytum.de>
2017-05-21 11:01:15 +0200

    Initialisation, streaming and collision added

--- WS2/collision.c
+++ WS2/collision.c
@@ -5,6 +5,29 @@ void computePostCollisionDistributions(double *currentCell, const double * const
 }
 
 void doCollision(double *collideField, int *flagField,const double * const tau,int xlength){
-  /* TODO */
+	/* TODO */
+	//Variable declaration
+	const int Cell_Vel_DOF = 19 ;
+	const int Dimensions = 3 ;
+	double Density;
+	double Velocity[Dimensions];
+	double Feq[Cell_Vel_DOF];
+	int X_Coordinate , Y_Coordinate , Z_Coordinate , Vel_Component , Current_Cell ;
+	
+	//Looping through individual element				
+	for( Z_Coordinate = 1 ; Z_Coordinate < xlength ; ++Z_Coordinate )  {
+		for( Y_Coordinate = 1 ; Y_Coordinate < xlength ; ++Y_Coordinate )  {
+			for( X_Coordinate = 1 ; X_Coordinate < xlength ; ++X_Coordinate ) {
+				Current_Cell =  ( Z_Coordinate * Square_xlength )
+										    + ( Y_Coordinate * xlength ) + X_Coordinate ;
+				if( flagField[Current_Cell] == 0 ) { 
+					computeDensity( Current_Cell , &Density );
+					computeVelocity( Current_Cell , &Density , Velocity );
+					computeFeq( &Density , Velocity , Feq );
+					computePostCollisionDistributions( Current_Cell , tau , Feq );
+				}
+			}
+		}
+	}
 }
 
--- WS2/initLB.c
+++ WS2/initLB.c
@@ -10,6 +10,110 @@ int readParameters(int *xlength, double *tau, double *velocityWall, int *timeste
 
 
 void initialiseFields(double *collideField, double *streamField, int *flagField, int xlength){
-  /* TODO */
+	/* TODO */
+	//Variable declaration
+	const int Cell_Vel_DOF = 19 ;
+	const int Dimensions = 3 ;
+	
+	int X_Coordinate , Y_Coordinate , Z_Coordinate , Vel_Component ;
+	int Absolute_Sum ;
+	double Current_Cell ; 
+	int Square_xlength = xlength * xlength ;
+
+	int CX[Vel_DOF] = { 0 , -1 , 0 , 1 , 0 , -1 , 0 , 1 , -1 ,
+						0 , 1 , -1 , 0 , 1 , 0 , -1 , 0 , 1 , 0 } ;
+	int CY[Vel_DOF] = { -1 , 0 , 0 , 0 , 1 , -1 , -1 , -1 , 0 ,
+						0 , 0 , 1 , 1 , 1 , -1 , 0 , 0 , 0 , 1 } ;
+	int CZ[Vel_DOF] = { -1 , -1 , -1 , -1 , -1 , 0 , 0 , 0 , 0 ,
+						 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 } ;
+	
+	double Initialization_Value_0 = 12.0 / 36.0 ;
+	double Initialization_Value_1 =  2.0 / 36.0 ;
+	double Initialization_Value_2 =  1.0 / 36.0 ;
+	double InitValue[Dimensions]  = { Initialization_Value_0 , Initialization_Value_1 , Initialization_Value_2 } ;
+	
+	
+	//all the fields are separately initialised for cache optimisation
+	//Initialization of collideField
+	for( Z_Coordinate = 0 ; Z_Coordinate <= xlength ; ++Z_Coordinate )  {
+		for( Y_Coordinate = 0 ; Y_Coordinate <= xlength ; ++Y_Coordinate )  {
+			for( X_Coordinate = 0 ; X_Coordinate <= xlength ; ++X_Coordinate ) {
+				Current_Cell =  ( Z_Coordinate * Square_xlength )
+										    + ( Y_Coordinate * xlength ) + X_Coordinate ;
+			/*	collideField[Current_Cell]      = Initialization_Value_2 ;
+				collideField[Current_Cell + 1]  = Initialization_Value_2 ;
+				collideField[Current_Cell + 2]  = Initialization_Value_1 ;
+				collideField[Current_Cell + 3]  = Initialization_Value_2 ;
+				collideField[Current_Cell + 4]  = Initialization_Value_2 ;
+				collideField[Current_Cell + 5]  = Initialization_Value_2 ;
+				collideField[Current_Cell + 6]  = Initialization_Value_1 ;
+				collideField[Current_Cell + 7]  = Initialization_Value_2 ;
+				collideField[Current_Cell + 8]  = Initialization_Value_1 ;
+				collideField[Current_Cell + 9]  = Initialization_Value_0 ;
+				collideField[Current_Cell + 10] = Initialization_Value_1 ;
+				collideField[Current_Cell + 11] = Initialization_Value_2 ;
+				collideField[Current_Cell + 12] = Initialization_Value_1 ;
+				collideField[Current_Cell + 13] = Initialization_Value_2 ;
+				collideField[Current_Cell + 14] = Initialization_Value_2 ;
+				collideField[Current_Cell + 15] = Initialization_Value_2 ;
+				collideField[Current_Cell + 16] = Initialization_Value_1 ;
+				collideField[Current_Cell + 17] = Initialization_Value_2 ;
+				collideField[Current_Cell + 18] = Initialization_Value_2 ;
+			*/
+				for( Vel_Component = 0 ; Vel_Component < Cell_Vel_DOF ; ++Vel_Component ){
+					Absolute_Sum = abs( CX[Vel_Component] ) + abs( CY[Vel_Component] ) + abs( CZ[Vel_Component] ) ;
+					collideField[Current_Cell + Vel_Component] = InitValue[Absolute_Sum] ;
+				}
+			}
+		}
+	}
+	
+	
+	//Initialization of streamField
+	for( Z_Coordinate = 0 ; Z_Coordinate <= xlength ; ++Z_Coordinate )  {
+		for( Y_Coordinate = 0 ; Y_Coordinate <= xlength ; ++Y_Coordinate )  {
+			for( X_Coordinate = 0 ; X_Coordinate <= xlength ; ++X_Coordinate ) {
+				Current_Cell =  ( Z_Coordinate * Square_xlength )
+										    + ( Y_Coordinate * xlength ) + X_Coordinate ;
+				streamField[Current_Cell]      = Initialization_Value_2 ;
+				streamField[Current_Cell + 1]  = Initialization_Value_2 ;
+				streamField[Current_Cell + 2]  = Initialization_Value_1 ;
+				streamField[Current_Cell + 3]  = Initialization_Value_2 ;
+				streamField[Current_Cell + 4]  = Initialization_Value_2 ;
+				streamField[Current_Cell + 5]  = Initialization_Value_2 ;
+				streamField[Current_Cell + 6]  = Initialization_Value_1 ;
+				streamField[Current_Cell + 7]  = Initialization_Value_2 ;
+				streamField[Current_Cell + 8]  = Initialization_Value_1 ;
+				streamField[Current_Cell + 9]  = Initialization_Value_0 ;
+				streamField[Current_Cell + 10] = Initialization_Value_1 ;
+				streamField[Current_Cell + 11] = Initialization_Value_2 ;
+				streamField[Current_Cell + 12] = Initialization_Value_1 ;
+				streamField[Current_Cell + 13] = Initialization_Value_2 ;
+				streamField[Current_Cell + 14] = Initialization_Value_2 ;
+				streamField[Current_Cell + 15] = Initialization_Value_2 ;
+				streamField[Current_Cell + 16] = Initialization_Value_1 ;
+				streamField[Current_Cell + 17] = Initialization_Value_2 ;
+				streamField[Current_Cell + 18] = Initialization_Value_2 ;
+			}
+		}
+	}
+	
+	
+	//Initialization of flagField
+	for( Z_Coordinate = 0 ; Z_Coordinate <= xlength ; ++Z_Coordinate )  {
+		for( Y_Coordinate = 0 ; Y_Coordinate <= xlength ; ++Y_Coordinate )  {
+			for( X_Coordinate = 0 ; X_Coordinate <= xlength ; ++X_Coordinate ) {
+				Current_Cell =  ( Z_Coordinate * Square_xlength )
+										    + ( Y_Coordinate * xlength ) + X_Coordinate ;
+				if( Z_Coordinate == 0 )
+					flagField[Current_Cell] = 2 ; 
+				else if(   X_Coordinate == 0 || X_Coordinate == xlength 
+						|| Y_Coordinate == 0 || Y_Coordinate == xlength )
+					flagField[Current_Cell] = 1 ;
+				else
+					flagField[Current_Cell] = 0 ;
+			}
+		}
+	}
 }
 

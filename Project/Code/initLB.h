b8f8f9558319a02c0ec74d6da26e225565ba19fb
Ravi Kislaya <kislaya@in.tum.de>
2017-06-29 19:58:46 +0200

    Before Ravil-Ravi Merging

--- Project/Code/initLB.h
+++ Project/Code/initLB.h
@@ -1,43 +1,20 @@
+#include <vector>
+#include <list>
+
+#include "helper.h"
+#include "DataStructure.h"
+
 #ifndef _INITLB_H_
 #define _INITLB_H_
-#include "helper.h"
 
 
 
-/* initialises the particle distribution functions and the flagfield */
-void initialiseFields_Step( double **collideField,
-						   double **streamField,
-						   int **flagField,
-						   int **IdField,
-                           unsigned* Length );
 
-void initialiseFields_PlaneShearFlow( double **collideField,
-							  		  double **streamField,
-							  	      int **flagField,
-						      	      int **IdField,
-                              		  unsigned* Length );
-
-
-void initialiseFields_LidDrivenCavity( double **collideField,
-                                       double **streamField,
-                                       int **flagField,
-                                       int **IdField,
-                                       unsigned* Length );
-
-void initialiseFields_TiltedPlate( const char *PLATE_TXT_FILE_NAME,
-								   double **collideField,
-								   double **streamField,
-								   int **flagField,
-								   int **IdField,
-								   unsigned* Length );
-
-
-void testField( double **collideField,
-                double **streamField,
-                int **flagField,
-                int **IdField,
-                unsigned* Length );
-
-
+void initialiseData( double** collideField,
+                     double** streamField,
+					 int** flagField,
+                     int** VtkID,
+                     std::vector<Fluid*> &FluidDomain,
+                     std::vector<BoundaryEntry*> &BoundaryElementVector );
 
 #endif
